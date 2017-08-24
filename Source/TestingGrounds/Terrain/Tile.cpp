e0b75b67ea5bb283f36233bea06fe01e6b0f58ea
Benji <benji@chezmoi.com>
2017-08-24 23:58:46 +0400

    TG69 : One hell of a challenge Actors are now spawning without overlapping each other (checking we have sufficient space to spawn actor)

--- Source/TestingGrounds/Terrain/Tile.cpp
+++ Source/TestingGrounds/Terrain/Tile.cpp
@@ -12,29 +12,50 @@ ATile::ATile()
 
 }
 
-void ATile::PlaceActors(TSubclassOf<AActor> ToSpawn, int MinSpawn, int MaxSpawn)
+void ATile::PlaceActors(TSubclassOf<AActor> ToSpawn, int MinSpawn, int MaxSpawn, float Radius)
+{
+	int ActorsToSpawn = FMath::RandRange(MinSpawn, MaxSpawn);
+	
+	for(size_t i = 0 ; i <= ActorsToSpawn ; i++) 
+	{
+		FVector SpawnPoint;
+		bool bFound = FindEmptyLocation(SpawnPoint, Radius);
+		if (bFound)
+		{
+			PlaceActor(ToSpawn, SpawnPoint);
+		}	
+	}
+}
+
+bool ATile::FindEmptyLocation(FVector &OutLocation, float Radius)
 {
 	FVector Min(0, -2000, 0);
 	FVector Max(4000, 2000, 0);
 	FBox BoxBounds(Min, Max);
-	int ActorsToSpawn = FMath::RandRange(MinSpawn, MaxSpawn);
-	
-	for(size_t i = 0 ; i <= ActorsToSpawn ; i++) //TODO remove magic number 20
+	const int MAX_TRIES = 100;
+	for (int i = 0; i <= MAX_TRIES; i++)
 	{
-		FVector SpawnPoint = FMath::RandPointInBox(BoxBounds);
-		AActor* Spawned = GetWorld()->SpawnActor(ToSpawn);
-		Spawned->SetActorRelativeLocation(SpawnPoint);
-		Spawned->AttachToActor(this, FAttachmentTransformRules(EAttachmentRule::KeepRelative, false));
+		FVector TestPoint = FMath::RandPointInBox(BoxBounds);
+		if (CanSpawnAt(TestPoint,Radius))
+		{
+			OutLocation = TestPoint;
+			return true;
+		}
 	}
+	return false;
+}
 
+void ATile::PlaceActor(TSubclassOf<AActor> ToSpawn, FVector SpawnPoint)
+{
+	AActor* Spawned = GetWorld()->SpawnActor(ToSpawn);
+	Spawned->SetActorRelativeLocation(SpawnPoint);
+	Spawned->AttachToActor(this, FAttachmentTransformRules(EAttachmentRule::KeepRelative, false));
 }
 
 // Called when the game starts or when spawned
 void ATile::BeginPlay()
 {
 	Super::BeginPlay();
-	CastSphere(GetActorLocation(), 300);
-
 }
 
 // Called every frame
@@ -44,21 +65,23 @@ void ATile::Tick(float DeltaTime)
 
 }
 
-bool ATile::CastSphere(FVector Location, float Radius)
+bool ATile::CanSpawnAt(FVector Location, float Radius)
 {
 	FHitResult HitResult;
+	FVector GlobalLocation = ActorToWorld().TransformPosition(Location);
 	bool HasHit = GetWorld()->SweepSingleByChannel(
 		HitResult,
-		Location,
-		Location,
+		GlobalLocation,
+		GlobalLocation,
 		FQuat::Identity, //~=0 rotation
 		ECollisionChannel::ECC_GameTraceChannel2,
 		FCollisionShape::MakeSphere(Radius)
 	);
 	//HasHit is false, return Red. HasHit is true return Green
 	// A ? B : C : Is A true, if yes return B, if not return C
-	FColor ResultColor = HasHit ? FColor::Red : FColor::Green; 
-	DrawDebugCapsule(GetWorld(), Location,0, Radius, FQuat::Identity, ResultColor, true, 100);
-	return HasHit;
+	FColor ResultColor = HasHit ? FColor::Red : FColor::Green;
+	DrawDebugCapsule(GetWorld(), GlobalLocation, 0, Radius, FQuat::Identity, ResultColor, true, 100);
+
+	return !HasHit;
 }
 
