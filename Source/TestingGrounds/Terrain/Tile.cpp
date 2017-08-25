ef8060a06d483131bb84e5fbb130ea5180119735
Benji <benji@chezmoi.com>
2017-08-25 13:59:54 +0400

    TG70: Spawning actors without having them overlapping each others Adding random rotation and scale parameter to the spawned actors to change size & rotation

--- Source/TestingGrounds/Terrain/Tile.cpp
+++ Source/TestingGrounds/Terrain/Tile.cpp
@@ -12,19 +12,21 @@ ATile::ATile()
 
 }
 
-void ATile::PlaceActors(TSubclassOf<AActor> ToSpawn, int MinSpawn, int MaxSpawn, float Radius)
+void ATile::PlaceActors(TSubclassOf<AActor> ToSpawn, int MinSpawn, int MaxSpawn, float Radius, float MinScale, float MaxScale)
 {
 	int ActorsToSpawn = FMath::RandRange(MinSpawn, MaxSpawn);
-	
 	//Spawning Actors loop
 	for(size_t i = 0 ; i <= ActorsToSpawn ; i++) 
 	{
 		FVector SpawnPoint;
+		float RandomScale = FMath::RandRange(MinScale,MaxScale);
 		//Checking we can spawn at this Spawnpoint
-		bool bFound = FindEmptyLocation(SpawnPoint, Radius);
+		bool bFound = FindEmptyLocation(SpawnPoint, Radius * RandomScale);
 		if (bFound)
 		{
-			PlaceActor(ToSpawn, SpawnPoint);
+			float RandomRotation = FMath::RandRange(-180.f, 180.f);
+			
+			PlaceActor(ToSpawn, SpawnPoint,RandomRotation,RandomScale);
 		}	
 	}
 }
@@ -51,11 +53,13 @@ bool ATile::FindEmptyLocation(FVector &OutLocation, float Radius)
 	return false;
 }
 
-void ATile::PlaceActor(TSubclassOf<AActor> ToSpawn, FVector SpawnPoint)
+void ATile::PlaceActor(TSubclassOf<AActor> ToSpawn, FVector SpawnPoint, float Rotation, float Scale)
 {
 	AActor* Spawned = GetWorld()->SpawnActor(ToSpawn);
 	Spawned->SetActorRelativeLocation(SpawnPoint);
 	Spawned->AttachToActor(this, FAttachmentTransformRules(EAttachmentRule::KeepRelative, false));
+	Spawned->SetActorRotation(FRotator(0, Rotation, 0));
+	Spawned->SetActorScale3D(FVector(Scale));
 }
 
 // Called when the game starts or when spawned
@@ -89,9 +93,6 @@ bool ATile::CanSpawnAt(FVector Location, float Radius)
 	);
 	//HasHit is true, return Red. HasHit is false return Green
 	// A ? B : C : Is A true, if yes return B, if not return C
-	FColor ResultColor = HasHit ? FColor::Red : FColor::Green;
-	DrawDebugCapsule(GetWorld(), GlobalLocation, 0, Radius, FQuat::Identity, ResultColor, true, 100);
-
 	return !HasHit;
 }
 
